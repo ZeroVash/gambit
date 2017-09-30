5057bc3ccc0d89a8f321e124185ff14f07b3be08
zerovash <tiagofcunha92@gmail.com>
2017-09-30 20:46:37 +0100

    Created the first game feed! Updated python script

--- 6d82255d-3350-4064-8948-d596f4f2b62f/scripts/actions.py
+++ 6d82255d-3350-4064-8948-d596f4f2b62f/scripts/actions.py
@@ -7,11 +7,14 @@ import itertools
 playerside = None
 sideflip = None
 diesides = 20
+ownLP = 100
+ownGold  = 5
 
 def clear(card, x = 0, y = 0):
     mute()
     card.target(False)
 
+	
 def setDie(group, x = 0, y = 0):
     mute()
     global diesides
@@ -20,11 +23,13 @@ def setDie(group, x = 0, y = 0):
       diesides = num
       dieFunct(diesides)
 
+	  
 def rollDie(group, x = 0, y = 0):
     mute()
     global diesides
     dieFunct(diesides)
 
+	
 def dieFunct(num):
     if num == 6:
       n = rnd(1, 6)
@@ -44,6 +49,69 @@ def dieFunct(num):
       n = rnd(1, num)
       notify("{} rolls {} on a {}-sided die.".format(me, n, num))
 
+	  
+def add_lp(group, x = 0, y = 0):
+	mute()
+	global ownLP
+	count = askInteger("Add how much LP?", 10)
+	if count == None: return
+	ownLP += count
+	notify("{} adds {} LP. Current: {}.".format(me, count, ownLP))
+
+	
+def remove_lp(group, x = 0, y = 0):
+	mute()
+	global ownLP
+	count = askInteger("Remove how many LP?", 10)
+	if count == None: return
+	ownLP -= count
+	notify("{} removes {} LP. Current: {}.".format(me, count, ownLP))
+
+	
+def add_gold(group, x = 0, y = 0):
+	mute()
+	global ownGold
+	count = askInteger("Add how much Gold?", 1)
+	if count == None: return
+	ownGold += count
+	notify("{} adds {} Gold. Current: {}.".format(me, count, ownGold))
+
+	  
+def remove_gold(group, x = 0, y = 0):
+	mute()
+	global ownGold
+	count = askInteger("Remove how much Gold?", 1)
+	if count == None: return
+	ownGold -= count
+	notify("{} removes {} Gold. Current: {}.".format(me, count, ownGold))
+
+ 
+def tap(card, x = 0, y = 0):
+	mute()
+	card.orientation ^= Rot90
+	if card.orientation & Rot90 == Rot90:
+		notify('{} taps {}.'.format(me, card))
+	else:
+		notify('{} untaps {}.'.format(me, card))
+
+		
+def play_flip(card, x = 0, y = 0):
+	mute()
+	card.moveToTable(0,0)
+	card.isFaceUp = False
+	notify('{} plays a card face/down.'.format(me))  
+
+	
+def flip(card, x = 0, y = 0):
+	mute()
+	if card.isFaceUp:
+		card.isFaceUp = False
+		notify('{} flips {}.'.format(me, card))
+	else:
+		card.isFaceUp = True
+		notify('{} flips {}.'.format(me, card))
+
+		
 def shuffle(group, x = 0, y = 0):
     mute()
     for card in group:
@@ -51,3 +119,20 @@ def shuffle(group, x = 0, y = 0):
             card.isFaceUp = False
     group.shuffle()
     notify("{} shuffled their {}".format(me, group.name))
+
+	
+def draw(group, x = 0, y = 0):
+    mute()
+    if len(group) == 0: return
+    card = group[0]
+    card.moveTo(card.owner.hand)
+    notify("{} draws a card.".format(me))
+
+	
+def drawX(group, x = 0, y = 0):
+    if len(group) == 0: return
+    mute()
+    count = askInteger("Draw how many cards?", 7)
+    if count == None: return
+    for card in group.top(count): card.moveTo(card.owner.hand)
+    notify("{} draws {} cards.".format(me, count))
